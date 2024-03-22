6e6622f22d32bed0de9d29b42188aec5fdff9ca5
john-roufaeil <john.flouis1@gmail.com>
2024-03-23 00:54:20 +0200

    refactor: change from js to ts

---
+++ backend/index.ts
@@ -0,0 +1,25 @@
+type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op: Operation) : number => {
+  switch(op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add or divide!');
+  }
+}
+
+try {
+  console.log(calculator(1, 0 , 'divide'));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
