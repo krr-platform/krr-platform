c0fb2a1a33134319e10b632d9651863a0cc64a7e
john-roufaeil <john.flouis1@gmail.com>
2024-03-23 17:11:52 +0200

    style: basic structure of app using typescript

---
+++ backend/src/models/types.ts
@@ -0,0 +1,5 @@
+// example
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
+
+// example
+export type Visibility = 'great' | 'good' | 'ok' | 'poor';
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+/* eslint-disable no-console */
+// import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hi </h1>
-    </div>
-  )
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <h1> Hi </h1>
+        </div>
+    );
 }
 
 export default App;
