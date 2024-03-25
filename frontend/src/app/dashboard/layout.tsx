f54cafdd5406d2010f43b1ca7feca251dc614171
john-roufaeil <john.flouis1@gmail.com>
2024-03-25 23:06:11 +0200

    feat: table of contents is sticky

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import SideNav from '@/app/components/dashboard/sidenav';
 export default function DashboardLayout({ children }:
     { children: React.ReactNode }): JSX.Element {
     return (
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="flex h-screen flex-col md:flex-row  md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
