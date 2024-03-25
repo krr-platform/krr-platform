1a235012bc9fbd27dc851d554171c6428319117b
john-roufaeil <john.flouis1@gmail.com>
2024-03-25 23:41:37 +0200

    feat: table of content is updated according to page

--- frontend/src/app/dashboard/explanations/anti-unification/page.tsx
+++ frontend/src/app/dashboard/explanations/anti-unification/page.tsx
@@ -1,8 +1,45 @@
+'use client';
+
 /* eslint-disable max-len */
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 export default function LessonPage() {
+    const [activeSection, setActiveSection] = useState<string | null>(null);
+    const sectionRefs = {
+        basics: useRef(null),
+        examples: useRef(null),
+        applications: useRef(null),
+        algorithms: useRef(null),
+        resources: useRef(null),
+    };
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        setActiveSection(entry.target.id);
+                    }
+                });
+            },
+            { rootMargin: '0px', threshold: 0.7 },
+        );
+
+        Object.values(sectionRefs).forEach((ref) => {
+            if (ref.current) {
+                observer.observe(ref.current);
+            }
+        });
+
+        return () => {
+            Object.values(sectionRefs).forEach((ref) => {
+                if (ref.current) {
+                    observer.unobserve(ref.current);
+                }
+            });
+        };
+    }, [sectionRefs]);
     return (
         <div className="max-w-4xl mx-auto flex w-full border-black">
             <div className="flex">
@@ -37,63 +74,173 @@ export default function LessonPage() {
                         </p>
                         <hr className="border-b border-gray-200 my-6 rounded" />
 
-                        <h2 className="text-xl font-semibold text-gray-900 mb-1" id="introduction">
-                            Basics of Anti-Unification
-                        </h2>
-                        <h3 className="text-l font-medium text-gray-900" id="">
-                            Definition of anti-unification
-                        </h3>
-                        <p className="mb-12">
-                            Anti-unification is a technique used in a lot of fields
-                            in computational logic. In broad terms, anti-unification
-                            allows us to find generalizations to symbols or statements,
-                            enabling us to abstract the differences between them
-                            and emphasize their commonalities.
-                        </p>
+                        <div ref={sectionRefs.basics} id="basics">
+                            <h2 className="text-xl font-semibold text-gray-900 mb-1">
+                                Basics of Anti-Unification
+                            </h2>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Definition
+                            </h3>
+                            <p className="mb-4">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Least General Generalizations (LGG)
+                            </h3>
+                            <p className="mb-4">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Syntax Trees
+                            </h3>
+                            <p className="mb-12">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                        </div>
 
-                        <h2 className="text-xl font-semibold text-gray-900 mb-1" id="example">
-                            Example
-                        </h2>
-                        <p className="mb-12">
-                            Anti-unification is a technique used in a lot of fields
-                            in computational logic. In broad terms, anti-unification
-                            allows us to find generalizations to symbols or statements,
-                            enabling us to abstract the differences between them
-                            and emphasize their commonalities.
-                        </p>
+                        <div ref={sectionRefs.examples} id="examples">
+                            <h2 className="text-xl font-semibold text-gray-900 mb-1">
+                                Examples
+                            </h2>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Example 1: ...
+                            </h3>
+                            <p className="mb-4">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Example 2: ...
+                            </h3>
+                            <p className="mb-12">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                        </div>
 
-                        <h2 className="text-xl font-semibold text-gray-900 mb-1" id="example1">
-                            Example
-                        </h2>
-                        <p className="mb-12">
-                            Anti-unification is a technique used in a lot of fields
-                            in computational logic. In broad terms, anti-unification
-                            allows us to find generalizations to symbols or statements,
-                            enabling us to abstract the differences between them
-                            and emphasize their commonalities.
-                        </p>
+                        <div ref={sectionRefs.applications} id="applications">
+                            <h2 className="text-xl font-semibold text-gray-900 mb-1">
+                                Applications
+                            </h2>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Code Refactoring
+                            </h3>
+                            <p className="mb-4">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Inductive Language Programming (ILP)
+                            </h3>
+                            <p className="mb-4">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Natural Language Processing (NLP)
+                            </h3>
+                            <p className="mb-12">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                        </div>
 
-                        <h2 className="text-xl font-semibold text-gray-900 mb-1" id="example2">
-                            Example
-                        </h2>
-                        <p className="mb-12">
-                            Anti-unification is a technique used in a lot of fields
-                            in computational logic. In broad terms, anti-unification
-                            allows us to find generalizations to symbols or statements,
-                            enabling us to abstract the differences between them
-                            and emphasize their commonalities.
-                        </p>
+                        <div ref={sectionRefs.algorithms} id="algorithms">
+                            <h2 className="text-xl font-semibold text-gray-900 mb-1">
+                                Algorithms
+                            </h2>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Idea
+                            </h3>
+                            <p className="mb-4">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Basic Implementation
+                            </h3>
+                            <p className="mb-4">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Complexity Discussion
+                            </h3>
+                            <p className="mb-4">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                            <h3 className="text-l font-medium text-gray-900">
+                                Optimzation
+                            </h3>
+                            <p className="mb-12">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                        </div>
+
+                        <div ref={sectionRefs.resources} id="resources">
+                            <h2 className="text-xl font-semibold text-gray-900 mb-1">
+                                Further Resources
+                            </h2>
+                            <p className="mb-12">
+                                Anti-unification is a technique used in a lot of fields
+                                in computational logic. In broad terms, anti-unification
+                                allows us to find generalizations to symbols or statements,
+                                enabling us to abstract the differences between them
+                                and emphasize their commonalities.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className="">
                     <nav className="sticky pl-8 z-10 flex-grow w-full top-0">
                         <p className="text-md pb-4 text-gray-700">Lesson Contents</p>
                         <ul>
-                            <li className="border-l-4 pb-1"><a className="hover:text-orange-500 pl-2 font-light text-sm" href="#introduction">Basics of Anti-unification</a></li>
-                            <li className="border-l-4 pb-1"><a className="hover:text-orange-500 pl-2 font-light text-sm" href="#introduction">Examples</a></li>
-                            <li className="border-l-4 pb-1"><a className="hover:text-orange-500 pl-2 font-light text-sm" href="#introduction">Applications</a></li>
-                            <li className="border-l-4 pb-1"><a className="hover:text-orange-500 pl-2 font-light text-sm" href="#introduction">Algorithms</a></li>
-                            <li className="border-l-4 pb-1"><a className="hover:text-orange-500 pl-2 font-light text-sm" href="#introduction">Further Resources</a></li>
+                            <li className={`border-l-4 pb-1 ${activeSection === 'basics' ? 'border-orange-500' : ''}`}><a className="hover:text-orange-500 pl-2 font-light text-sm" href="#basics">Basics of Anti-unification</a></li>
+                            <li className={`border-l-4 pb-1 ${activeSection === 'examples' ? 'border-orange-500' : ''}`}><a className="hover:text-orange-500 pl-2 font-light text-sm" href="#examples">Examples</a></li>
+                            <li className={`border-l-4 pb-1 ${activeSection === 'applications' ? 'border-orange-500' : ''}`}><a className="hover:text-orange-500 pl-2 font-light text-sm" href="#applications">Applications</a></li>
+                            <li className={`border-l-4 pb-1 ${activeSection === 'algorithms' ? 'border-orange-500' : ''}`}><a className="hover:text-orange-500 pl-2 font-light text-sm" href="#algorithms">Algorithms</a></li>
+                            <li className={`border-l-4 pb-1 ${activeSection === 'resources' ? 'border-orange-500' : ''}`}><a className="hover:text-orange-500 pl-2 font-light text-sm" href="#resources">Further Resources</a></li>
                         </ul>
                     </nav>
                 </div>
