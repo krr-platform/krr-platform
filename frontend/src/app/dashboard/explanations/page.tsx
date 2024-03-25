7dd99ca47702eb19103d1901c13b6af35ffd7493
john-roufaeil <john.flouis1@gmail.com>
2024-03-25 21:41:28 +0200

    feat: basic lesson structure

--- frontend/src/app/dashboard/explanations/page.tsx
+++ frontend/src/app/dashboard/explanations/page.tsx
@@ -14,19 +14,6 @@ export default function ExplanationsPage() {
             </p>
             <hr className="border-b border-gray-200 my-6 rounded" />
             <List />
-            {/* <div className="space-y-8">
-                <div className="">
-                    <Image
-                        src="/../../../../../public/tool-logo.png" // Replace with your image path
-                        width={300}
-                        height={200}
-                        alt="Anti-Unification"
-                        className="mx-auto" // Center the image
-                        style={{ width: '300px', height: '200px' }} // Adjust size as needed
-                    />
-                    <h2 className="text-xl text-gray-800 mt-2">Anti-Unification</h2>
-                </div>
-            </div> */}
         </div>
     );
 }
