1b25d6f7e4d4153d9507e7137dc71d377f991df5
john-roufaeil <john.flouis1@gmail.com>
2024-03-25 21:41:28 +0200

    feat: basic lesson structure

--- frontend/src/app/components/dashboard/nav-links.tsx
+++ frontend/src/app/components/dashboard/nav-links.tsx
@@ -42,16 +42,15 @@ export default function NavLinks() {
                         className={clsx(
                             'flex h-[48px] grow items-center justify-center',
                             'gap-2 rounded-md bg-gray-50 p-3',
-                            'text-sm font-medium hover:bg-blue-50',
+                            'text-sm font-medium hover:bg-blue-100',
                             'hover:text-blue-500 md:flex-none',
                             'md:justify-start md:p-2 md:px-3',
                             {
-                                'bg-blue-50 text-blue-500':
-                                    pathname === link.href,
+                                'bg-blue-100 text-blue-500': pathname
+                                    ? pathname.includes(link.href) : false,
                             },
                         )}
                     >
-
                         <LinkIcon className="w-6" />
                         <p className="hidden md:block">{link.name}</p>
                     </Link>
--- frontend/src/app/dashboard/explanations/[topic]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LessonPage() {
-    return (
-        <div>
-            LessonPage
-        </div>
-    );
-}
