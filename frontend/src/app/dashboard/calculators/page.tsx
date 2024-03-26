aef78688c3d2974f6d983f4a26cf9abe94160786
john-roufaeil <john.flouis1@gmail.com>
2024-03-26 22:59:26 +0200

    feat: add calculator abstract ui

--- frontend/src/app/dashboard/calculators/page.tsx
+++ frontend/src/app/dashboard/calculators/page.tsx
@@ -1,9 +1,19 @@
+// import Image from 'next/image';
 import React from 'react';
+import ListCalculators from '@/app/components/dashboard/list-calculators';
 
-export default function CalculatorsPage() {
+export default function CalculatorssPage() {
     return (
-        <div>
-            CalculatorsPage
+        <div className="max-w-4xl mx-auto py-10 px-4">
+            <h1 className="text-3xl font-bold  text-gray-900 mb-2">
+                Calculators Module
+            </h1>
+            <p className="">
+                Effortlessly calculate AI-related computations with
+                interactive results.
+            </p>
+            <hr className="border-b border-gray-200 my-6 rounded" />
+            <ListCalculators />
         </div>
     );
 }
