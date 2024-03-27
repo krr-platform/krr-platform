27bfd85c51c3ba3f4c2428e52c44e2e2d10a8b25
john-roufaeil <john.flouis1@gmail.com>
2024-03-27 17:42:03 +0200

    fix: silence errors

--- frontend/src/app/dashboard/calculators/anti-unification-first-order-language/page.tsx
+++ frontend/src/app/dashboard/calculators/anti-unification-first-order-language/page.tsx
@@ -43,16 +43,18 @@ export default function LessonPage() {
                             <p className="font-medium text-center mb-4"> Calculate anti-unification to first-order language statements </p>
                             <div className="flex items-center">
                                 <p className="mr-2">Usage</p>
-                                <button><InformationCircleIcon className="w-5 h-5" /></button>
+                                <button type="button" aria-label="Usage Help">
+                                    <InformationCircleIcon className="w-5 h-5" />
+                                </button>
                             </div>
                             <input placeholder="Input first-order statement..." className="w-full rounded-lg my-2 p-2 border-2" />
                             <input placeholder="Input first-order statement..." className="w-full rounded-lg my-2 p-2 border-2" />
-                            <button className="border-2 border-blue-300 text-blue-500 hover:bg-blue-100 hover:border-blue-500 text-bold font-medium py-2 px-4 my-2 rounded transition-colors duration-300"> Add Statement </button>
+                            <button type="button" className="border-2 border-blue-300 text-blue-500 hover:bg-blue-100 hover:border-blue-500 text-bold font-medium py-2 px-4 my-2 rounded transition-colors duration-300"> Add Statement </button>
                             <p>  </p>
                             <p>  </p>
                             <p>  </p>
-                            <div class="flex justify-end items-center">
-                                <button onClick={() => { }} className="bg-blue-500 border-2 text-white rounded-lg py-2 px-4 hover:bg-blue-700 mt-2 text-bold font-medium transition-colors duration-300">Calculate</button>
+                            <div className="flex justify-end items-center">
+                                <button type="button" onClick={() => { }} className="bg-blue-500 border-2 text-white rounded-lg py-2 px-4 hover:bg-blue-700 mt-2 text-bold font-medium transition-colors duration-300">Calculate</button>
                             </div>
                         </div>
 
