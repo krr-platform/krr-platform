25c5a05accd308344c62fa6e3ede044b84f4d453
john-roufaeil <john.flouis1@gmail.com>
2024-03-31 15:23:23 +0200

    feat: add games basic UI page, enumerate lists of calcs and explanations

--- frontend/src/app/components/dashboard/list-explanations.tsx
+++ frontend/src/app/components/dashboard/list-explanations.tsx
@@ -3,8 +3,6 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
 import Link from 'next/link';
 
-import { ArrowRightCircleIcon } from '@heroicons/react/24/outline';
-
 interface Item {
     id: number;
     name: string;
@@ -46,12 +44,13 @@ export default function ListExplanations() {
                         <div className="flex min-w-0 gap-x-4 group justify-between w-full">
                             <div className="p-2 pl-5 flex min-w-0 gap-x-4 border-2 border-white w-full group-hover:bg-orange-50 group-hover:border-orange-500 group-hover:rounded transition-colors duration-300">
                                 <div className="min-w-0 flex-auto">
-                                    <p className="text-base font-semibold leading-7 text-gray-950 group-hover:text-orange-500 transition-colors duration-300">{item.name}</p>
+                                    <p className="text-base font-semibold leading-7 text-gray-950 group-hover:text-orange-500 transition-colors duration-300">
+                                        {item.id}
+                                        .
+                                        {' '}
+                                        {item.name}
+                                    </p>
                                     <p className="mt-1 text-sm leading-5 text-gray-500">{item.shortDesc}</p>
-                                </div>
-                                <div className="group-hover:hidden flex-none rounded m-auto bg-white h-10 w-10 justify-end transition-colors duration-300" />
-                                <div className="hidden group-hover:block flex-none rounded m-auto bg-orange-500 h-10 w-10 justify-end transition-colors duration-300">
-                                    <ArrowRightCircleIcon className="text-orange-50" w-10 h-10 />
                                 </div>
                             </div>
                         </div>
--- frontend/src/app/dashboard/games/page.tsx
+++ frontend/src/app/dashboard/games/page.tsx
@@ -1,9 +1,16 @@
+// import Image from 'next/image';
 import React from 'react';
 
-export default function GamesPage() {
+export default function CalculatorssPage() {
     return (
-        <div>
-            GamesPage
+        <div className="max-w-4xl mx-auto py-10 px-4">
+            <h1 className="text-3xl font-bold  text-gray-900 mb-2">
+                Games Module
+            </h1>
+            <p className="">
+                Fun AI-related games to reinforce learned material.
+            </p>
+            <hr className="border-b border-gray-200 my-6 rounded" />
         </div>
     );
 }
