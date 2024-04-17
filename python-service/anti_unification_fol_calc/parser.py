ca4005a926688a9d872bac9ffe5b2d02ee83c160
john-roufaeil <john.flouis1@gmail.com>
2024-04-17 17:26:55 +0200

    feat: parser complete

--- python-service/anti_unification_fol_calc/parser.py
+++ python-service/anti_unification_fol_calc/parser.py
@@ -27,6 +27,24 @@ class Node:
             return f"{self.value}({', '.join(child.stringify() if isinstance(child, Node) else str(child) for child in self.children)})"
 
 
+def getPrecedence(token):
+    if token['type'] == 'LEFT_SQUARE':
+        return 0
+    elif token['type'] in ['UNIVERSAL_QUANTIFIER', 'EXISTENTIAL_QUANTIFIER']:
+        return 1
+    elif token['type'] == 'LOGICAL_NEG':
+        return 2
+    elif token['type'] == 'LOGICAL_AND':
+        return 3
+    elif token['type'] == 'LOGICAL_OR':
+        return 4
+    elif token['type'] == 'LOGICAL_IMPLICATION':
+        return 5
+    elif token['type'] == 'LOGICAL_EQUIVALENT':
+        return 6
+    return 10
+
+
 def parse(tokens):
     operands_stack = []
     operators_stack = []
@@ -34,7 +52,7 @@ def parse(tokens):
 
     while tokens:
         token = tokens.pop(0)
-
+        print('DEBUG:', token)
         # variable or constant -> simple push
         if token['type'] in ['VARIABLE', 'CONSTANT']:
             operands_stack.append(token)
@@ -43,9 +61,11 @@ def parse(tokens):
         elif token['type'] in ['FUNCTION', 'PREDICATE']:
             fn_pd_stack.append(token)
             operands_stack.append('(')
+
         # commas -> simple push
         elif token['type'] == 'COMMA':
             operands_stack.append(token)
+
         # end of fn or pd -> pop name, pop all till (, combine and push
         elif token['type'] == 'RIGHT_PAREN':
             fn_or_pred = fn_pd_stack.pop()
@@ -61,92 +81,67 @@ def parse(tokens):
             operands_stack.append(fn_or_pred)
 
         elif token['type'] == 'LEFT_SQUARE':
-            operators_stack.append(token)
+            operators_stack.append('[')
+
         elif token['type'] == 'RIGHT_SQUARE':
-            operators_stack.append(token)
-        # elif token['type'] in
-        #     operands_stack.push('[')
-    print(operands_stack)
-    print(operators_stack)
-    print(fn_pd_stack)
-    # while operands_stack:
+            idx = len(operators_stack) - 1 - operators_stack[::-1].index('[')
+            while len(operators_stack) != idx + 1:
+                operator = operators_stack.pop()
+                if operator['type'] != 'LOGICAL_NEG':
+                    operand2 = operands_stack.pop()
+                    operand1 = operands_stack.pop()
+                    operator['children'] = [operand1, operand2]
+                    operands_stack.append(operator)
+                else:
+                    operand = operands_stack.pop()
+                    operator['children'] = [operand]
+                    operands_stack.append(operator)
+            operators_stack.pop()
+            operator = operators_stack.pop()
+            if operator['type'] != 'LOGICAL_NEG':
+                operand2 = operands_stack.pop()
+                operand1 = operands_stack.pop()
+                operator['children'] = [operand1, operand2]
+                operands_stack.append(operator)
+            else:
+                operand = operands_stack.pop()
+                operator['children'] = [operand]
+                operands_stack.append(operator)
 
-    # if not tokens:
-    #     return None
+        elif token['type'] in ['LOGICAL_AND', 'LOGICAL_OR', 'LOGICAL_IMPLICATION', 'LOGICAL_EQUIVALENT',
+                               'UNIVERSAL_QUANTIFIER', 'EXISTENTIAL_QUANTIFIER', 'LOGICAL_NEG']:
+            if (len(operators_stack) == 0) or (getPrecedence(token) <= getPrecedence(operands_stack[-1])):
+                operators_stack.append(token)
+            else:
+                operator = operators_stack.pop()
+                if operator['type'] != 'LOGICAL_NEG':
+                    operand2 = operands_stack.pop()
+                    operand1 = operands_stack.pop()
+                    operator['children'] = [operand1, operand2]
+                    operands_stack.append(operator)
+                else:
+                    operand = operands_stack.pop()
+                    operator['children'] = [operand]
+                    operands_stack.append(operator)
+                operators_stack.append(token)
+        print('OPERANDS', operands_stack)
+        print('OPERATORS:', operators_stack)
+        print('FN:', fn_pd_stack)
+        print()
 
-    # operators = {
-    #     'LOGICAL_AND': 1,
-    #     'LOGICAL_OR': 2,
-    #     'LOGICAL_IMPLICATION': 3,
-    #     'LOGICAL_EQUIVALENT': 4
-    # }
+    while len(operands_stack) > 1:
+        operator = operators_stack.pop()
+        if operator['type'] != 'LOGICAL_NEG':
+            operand2 = operands_stack.pop()
+            operand1 = operands_stack.pop()
+            operator['children'] = [operand1, operand2]
+            operands_stack.append(operator)
+        else:
+            operand = operands_stack.pop()
+            operator['children'] = [operand]
+            operands_stack.append(operator)
 
-    # operator_stack = []
-    # while tokens:
-    #     token = tokens.pop(0)
-
-    #     if token['type'] in ['FUNCTION', 'PREDICATE']:
-    #         node = Node(token['type'], token['value'], children=[])
-    #         while tokens[0]['type'] != 'RIGHT_PAREN':
-    #             child = parse(tokens)
-    #             node.children.append(child)
-    #             if tokens[0]['type'] == 'COMMA':
-    #                 tokens.pop(0)  # consume comma
-    #         tokens.pop(0)  # consume right parenthesis
-
-    #     elif token['type'] in ['UNIVERSAL_QUANTIFIER', 'EXISTENTIAL_QUANTIFIER']:
-    #         node = Node(token['type'], token['value'],
-    #                 children=[parse(tokens), parse(tokens)])
-
-    #     elif token['type'] == 'LOGICAL_NEG':
-    #         node = Node(token['type'], token['value'],
-    #                     children=[parse(tokens)])
-
-    #     elif token['type'] in ['VARIABLE', 'CONSTANT']:
-    #         node = Node(token['type'], token['value'])
-
-    #     elif token['type'] in operators:
-    #         while operator_stack and operator_stack[-1]['type'] != 'LEFT_PAREN' and operators.get(operator_stack[-1]['type'], 0) >= operators.get(token['type'], 0):
-    #             apply_operator(output, operator_stack.pop())
-    #         operator_stack.append(token)
-
-    #     elif token['type'] in ['LEFT_PAREN']:
-    #         operator_stack.append(token)
-
-    #     elif token['type'] in ['RIGHT_PAREN']:
-    #         while operator_stack[-1]['type'] != 'LEFT_PAREN':
-    #             apply_operator(output, operator_stack.pop())
-    #         operator_stack.pop()  # Discard the left parenthesis
-
-    #     else:
-    #         return None
-
-    # while operator_stack:
-    #     apply_operator(output, operator_stack.pop())
-
-    # return output[0].serialize()
-
-    # token = tokens.pop(0)
-    # if token['type'] in ['FUNCTION', 'PREDICATE']:
-    #     node = Node(token['type'], token['value'], children=[])
-    #     while tokens[0]['type'] != 'RIGHT_PAREN':
-    #         child = parse(tokens)
-    #         node.children.append(child)
-    #         if tokens[0]['type'] == 'COMMA':
-    #             tokens.pop(0)  # consume comma
-    #     tokens.pop(0)  # consume right parenthesis
-    # elif token['type'] in ['UNIVERSAL_QUANTIFIER', 'EXISTENTIAL_QUANTIFIER', 'LOGICAL_AND', 'LOGICAL_OR', 'LOGICAL_EQUIVALENT', 'LOGICAL_IMPLICATION']:
-    #     node = Node(token['type'], token['value'],
-    #                 children=[parse(tokens), parse(tokens)])
-    # elif token['type'] == 'LOGICAL_NEG':
-    #     node = Node(token['type'], token['value'],
-    #                 children=[parse(tokens)])
-    # elif token['type'] in ['VARIABLE', 'CONSTANT']:
-    #     node = Node(token['type'], token['value'])
-    # else:
-    #     return None
-
-    # return node.serialize()
+    return operands_stack.pop()
 
 
 def parseAllTokens(tokensLists):
