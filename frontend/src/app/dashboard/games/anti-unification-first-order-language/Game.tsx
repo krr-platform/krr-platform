4dcff4190da752580ce49d6d2c8e97f006e9e7fa
john-roufaeil <john.flouis1@gmail.com>
2024-05-09 17:48:31 +0300

    feat: extract questions in a json

--- frontend/src/app/dashboard/games/anti-unification-first-order-language/Game.tsx
+++ frontend/src/app/dashboard/games/anti-unification-first-order-language/Game.tsx
@@ -1,27 +1,22 @@
+/* eslint-disable react/function-component-definition */
+/* eslint-disable react/button-has-type */
+/* eslint-disable max-len */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
+import questions from './questions.json';
 
-interface Question {
-    text: string;
-    choices: string[];
-    correctChoiceIndex: number;
-    body?: string;
-    imageUrl?: string;
-}
+// interface Question {
+//     id: number;
+//     text: string;
+//     choices: string[];
+//     correctChoiceIndex: number;
+//     body?: string;
+//     imageUrl?: string;
+// }
 
-const questions: Question[] = [
-    {
-        text: 'Question 1?',
-        choices: ['A', 'B', 'C', 'D'],
-        correctChoiceIndex: 0,
-    },
-    // Add more questions here
-];
-
-// eslint-disable-next-line react/function-component-definition
-const Game: React.FC = () => {
+export default function Game() {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [score, setScore] = useState(0);
     const [showCorrect, setShowCorrect] = useState(false);
@@ -33,37 +28,47 @@ const Game: React.FC = () => {
         if (selectedChoiceIndex === currentQuestion.correctChoiceIndex) {
             setScore(score + 10);
             setShowCorrect(true);
+            setTimeout(() => {
+                setShowCorrect(false);
+                setCurrentQuestionIndex(currentQuestionIndex + 1);
+            }, 1500);
         } else {
             setShowIncorrect(true);
+            setTimeout(() => {
+                setShowIncorrect(false);
+                setCurrentQuestionIndex(currentQuestionIndex + 1);
+            }, 1500);
         }
-
-        setTimeout(() => {
-            setShowCorrect(false);
-            setShowIncorrect(false);
-            setCurrentQuestionIndex(currentQuestionIndex);
-        }, 1500);
     };
 
     return (
-        <div className="rounded-lg w-full p-8 shadow-lg border-2">
-            <h1>
-                {currentQuestion.text}
-            </h1>
-            <ul>
-                {currentQuestion.choices.map((choice, index) => (
-                    <li key={index} onClick={() => handleAnswer(index)}>
-                        {choice}
-                    </li>
-                ))}
-            </ul>
-            {showCorrect && <div>Correct!</div>}
-            {showIncorrect && <div>Incorrect!</div>}
-            <div>
-                Score:&nbsp;
-                {score}
+        <div className="px-4">
+            <div className="rounded-lg w-full p-8 shadow-lg border-2">
+                <div className="text-right">
+                    Score:&nbsp;
+                    {score}
+                </div>
+                <h1 className="text-center font-medium text-lg pb-4 mb-8 border-b-2">
+                    {currentQuestion.text}
+                </h1>
+                <div className="grid grid-cols-2 gap-4">
+                    {currentQuestion.choices.map((choice, index) => (
+                        <button
+                            className="border-2
+                            hover:text-blue-500 hover:bg-blue-50
+                            hover:border-blue-500 text-bold
+                            font-medium p-2 rounded-lg
+                            transition-colors duration-300 max-w-x"
+                            key={index}
+                            onClick={() => handleAnswer(index)}
+                        >
+                            {choice}
+                        </button>
+                    ))}
+                </div>
+                {showCorrect && <div>Correct!</div>}
+                {showIncorrect && <div>Incorrect!</div>}
             </div>
         </div>
     );
-};
-
-export default Game;
+}
