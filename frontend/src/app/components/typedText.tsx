9f2f77400727acee75da9aa80341ba981c1a3e36
john-roufaeil <john.flouis1@gmail.com>
2024-06-04 13:05:27 +0200

    fix: explanation mistake

---
+++ frontend/src/app/components/typedText.tsx
@@ -0,0 +1,30 @@
+// components/TypedText.js
+import Typed from 'typed.js';
+import { useEffect, useRef } from 'react';
+
+const TypedText = () => {
+    const el = useRef(null);
+
+    useEffect(() => {
+        const options = {
+            strings: [
+                'Here you can add your strings',
+                'Another string example'
+            ],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true
+        };
+
+        const typed = new Typed(el.current, options);
+
+        return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+    }, []);
+
+    return <span ref={el} />;
+};
+
+export default TypedText;
