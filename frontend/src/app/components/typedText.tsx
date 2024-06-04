59e0126c6ec74d4373f9680392005180124a6328
john-roufaeil <john.flouis1@gmail.com>
2024-06-04 13:13:36 +0200

    style: comment currently unused code

--- frontend/src/app/components/typedText.tsx
+++ frontend/src/app/components/typedText.tsx
@@ -1,30 +1,30 @@
-// components/TypedText.js
-import Typed from 'typed.js';
-import { useEffect, useRef } from 'react';
+// // components/TypedText.js
+// import Typed from 'typed.js';
+// import { useEffect, useRef } from 'react';
 
-const TypedText = () => {
-    const el = useRef(null);
+// const TypedText = () => {
+//     const el = useRef(null);
 
-    useEffect(() => {
-        const options = {
-            strings: [
-                'Here you can add your strings',
-                'Another string example'
-            ],
-            typeSpeed: 50,
-            backSpeed: 50,
-            loop: true
-        };
+//     useEffect(() => {
+//         const options = {
+//             strings: [
+//                 'Here you can add your strings',
+//                 'Another string example'
+//             ],
+//             typeSpeed: 50,
+//             backSpeed: 50,
+//             loop: true
+//         };
 
-        const typed = new Typed(el.current, options);
+//         const typed = new Typed(el.current, options);
 
-        return () => {
-            // Destroy Typed instance during cleanup to stop animation
-            typed.destroy();
-        };
-    }, []);
+//         return () => {
+//             // Destroy Typed instance during cleanup to stop animation
+//             typed.destroy();
+//         };
+//     }, []);
 
-    return <span ref={el} />;
-};
+//     return <span ref={el} />;
+// };
 
-export default TypedText;
+// export default TypedText;
